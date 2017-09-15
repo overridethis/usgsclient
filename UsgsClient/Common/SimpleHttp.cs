95c27f4bfc68170146fd167ae618509666bb0f9f
Roberto Hernandez <roberto.hernandez@infernored.com>
2017-09-14 21:26:58 -0400

    Fighting with cross platform dependency System.Net.Http

--- UsgsClient/Common/SimpleHttp.cs
+++ UsgsClient/Common/SimpleHttp.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-#if NET46
-using RestSharp;
-#else
 using System.Net.Http;
-#endif
 
 namespace UsgsClient.Common
 {
@@ -19,7 +15,6 @@ namespace UsgsClient.Common
             Dictionary<string, string> headers = null) 
         {
             var content = string.Empty;
-#if NETSTANDARD1_1
             using (var client = new HttpClient())
             {
                 var req = new HttpRequestMessage()
@@ -36,16 +31,6 @@ namespace UsgsClient.Common
                 // TODO: Error Handling.
                 content = await rsp.Content.ReadAsStringAsync();
             }   
-#else
-            var client = new RestClient();
-            var req = new RestRequest(uri);
-
-            foreach (var header in headers ?? EmptyDictionary)
-                req.AddHeader(header.Key, header.Value);
-
-            var rsp = await client.ExecuteGetTaskAsync(req);
-            content = rsp.Content;
-#endif
             return JsonConvert.DeserializeObject<TResponseType>(content);
         }
     }
